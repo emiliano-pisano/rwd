37faa9ec734a38fca6fe416f28e910370c7b8429
emiliano-pisano <Wored1943@dayrep.com>
2017-03-26 17:04:23 +0200

    Create main.js

---
+++ css/js/main.js
@@ -0,0 +1,5 @@
+var lorem="Lorem ipsum dolor sit amet, quis facilisi luctus. Donec elit vestibulum accumsan, neque porta, augue quam sed, duis eu urna eget suspendisse donec, tincidunt mauris mi tortor. Nec nunc tortor semper tristique, wisi sit, quam orci duis diam etiam. Vitae montes pellentesque volutpat rutrum sed duis, turpis sit diam amet velit, curabitur eget, mauris elementum ut ea. Id purus eget magna nunc justo, et fermentum dui egestas wisi habitant, non quam aliquam posuere elementum lacus velit, semper ut a lacinia mauris. Pede maecenas odio aenean excepturi, donec tempus dolor amet dictum proin consequat, cras et sed. Et vestibulum class interdum, nec rutrum, lorem pharetra, quisque tincidunt at donec aliquam vestibulum cum. Vulputate urna mauris nunc nec libero pharetra. Diam aliquam fermentum quam risus, in vehicula ligula iaculis.";
+lorem=lorem + lorem+lorem+lorem;
+$('.lorem1').html (lorem);
+$('.lorem2').html (lorem);
+$('.lorem3').html (lorem);
